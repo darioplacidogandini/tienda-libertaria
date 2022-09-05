14e5cf4020142d76edac33a15d5d3c29109ba137
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-09-05 00:14:31 -0300

    .

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
       <header className="App-header">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Partido_Libertario_%28Argentina%29.png/800px-Partido_Libertario_%28Argentina%29.png" 
         className="App-logo" alt="logo"/>
-        <p>
-          Tienda Libertaria Corrientes
-        </p>
+        <h1>Tienda Libertaria Corrientes</h1>
         <ProductsContainer/>
       </header>
     </div>
