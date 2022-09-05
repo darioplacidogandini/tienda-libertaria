a83da911625e82099425e95f315665f83ac3134f
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-09-05 19:36:23 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/bootstrap.min.css';
 import ProductsContainer from './container/products.container';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Partido_Libertario_%28Argentina%29.png/800px-Partido_Libertario_%28Argentina%29.png" 
         className="App-logo" alt="logo"/>
-        <h1>Tienda Libertaria Corrientes</h1>
+        <h1 class="text-primary">Tienda Libertaria Corrientes</h1>
       </header>
       <ProductsContainer/>
     </div>
