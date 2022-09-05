4ff8e465b61f36734151d4320e6e0d377ac91718
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-09-05 14:19:11 -0300

    .

--- src/container/products.container.jsx
+++ src/container/products.container.jsx
@@ -1,9 +1,15 @@
 import CardComponent from '../component/card.component';
+import data from '../assets/products.json'
 
 const ProductsContainer = () => {
+    const products = data.products;
     return (
         <div className='container'>
-            <CardComponent/>
+            <div class="row">
+            {products.map((products)=>(
+                <CardComponent/>
+            ))}
+            </div>
         </div>
     )
 }
