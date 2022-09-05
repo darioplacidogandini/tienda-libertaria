388366542dea313ea96513f2207bdc03e2ff1f78
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-09-04 21:51:53 -0300

    .

--- src/component/card.component.jsx
+++ src/component/card.component.jsx
@@ -1,11 +1,11 @@
 const CardComponent = () => {
     return (
         <div class="card col-4">
-        <img src="..." class="card-img-top" alt="..."/>
+        <img src="/src/assets/producto.jpg" class="card-img-top" alt="..."/>
         <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">TITULO PRODUCTO</h5>
+        <p class="card-text">Descripcion del producto</p>
+        <p class="card-text">$1200</p>
         </div>
         </div>
     )
