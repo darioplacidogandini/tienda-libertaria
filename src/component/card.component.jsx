5d7a3a96637d2de4af8263ec7b68791159a67614
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-09-06 12:26:07 -0300

    .

--- src/component/card.component.jsx
+++ src/component/card.component.jsx
@@ -2,7 +2,24 @@ function CardComponent({products}) {
     return (
         <div className='container'>
             <div class="card col-sm x-2 py-2">
-                <img src={products.image} class="card-img-top" alt="Imagen del producto"/>
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src={products.image1} alt="Imagen del producto 1"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src={products.image2} alt="Imagen del producto 2"/>
+                    </div>
+                </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Siguiente</span>
+            </a>
+            </div>
                 <div class="card-body"></div>
                     <h5 class="card-title text-primary">{products.product}</h5>
                     <p class="card-text">{products.description}</p>
